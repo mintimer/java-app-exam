350c38ab2b2f5556f33cdaf4d49805ea7e3dd72f
Settapong Supkong <settapong.s@kbtg.tech>
2021-06-30 13:57:18 +0700

    main function

--- src/main/java/com/example/demo/GetData.java
+++ src/main/java/com/example/demo/GetData.java
@@ -1,8 +1,12 @@
 package com.example.demo;
 
+import org.jcp.xml.dsig.internal.dom.ApacheData;
+import org.jcp.xml.dsig.internal.dom.ApacheTransform;
+
 public class GetData {
     public static void main(String[] args) {
         AppGateway appGateway = new AppGateway();
         appGateway.getResponseItem(args);
+        //Convert to CSV/Excel
     }
 }
